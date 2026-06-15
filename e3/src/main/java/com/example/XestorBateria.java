7e0891f495e750ddbd750f71be2b9857c7193f6f
valeriavf23 <a23valeriavf@iessanclemente.net>
2026-06-15 10:33:35 +0200

    javadoc3

--- e3/src/main/java/com/example/XestorBateria.java
+++ e3/src/main/java/com/example/XestorBateria.java
@@ -1,13 +1,25 @@
 package com.example;
 
+/*
+Se crea la clase XestorBateria
+ */
 public class XestorBateria {
 
+    /*
+Contiene un int nivelCarga
+ */
     private int nivelCarga;
 
+    /*
+El nivelCarga va a ser igual a cargaInicial
+*/
     public XestorBateria(int cargaInicial) {
         nivelCarga = cargaInicial;
     }
 
+    /*
+Si la cantidad es mayor a cero se le va a sumar a nivelCarga la cantidad dicha
+*/
     public void cargar(int cantidade) {
 
         if (cantidade > 0) {
@@ -16,6 +28,9 @@ public class XestorBateria {
 
     }
 
+    /*
+Si la cantidad es mayor a cero se le va a restar a nivelCarga la cantidad dicha
+*/
     public void descargar(int cantidade) {
 
         if (cantidade > 0) {
@@ -24,16 +39,25 @@ public class XestorBateria {
 
     }
 
+    /*
+Devuelve nivelCarga
+*/
     public int getNivelCarga() {
         return nivelCarga;
     }
 
+    /*
+Si tiene 0 porciento devuelve nivelCarga
+*/
     public boolean estaBaleira() {
 
         return nivelCarga <= 0;
 
     }
 
+    /*
+Si la funcion estaBaleira es cierta devuelve eso si no avisa cuando está por debajo de 20
+*/
     public boolean necesitaCarga() {
 
         if (estaBaleira()) {
@@ -43,6 +67,14 @@ public class XestorBateria {
         return nivelCarga < 20;
     }
 
+    /*
+        Se crea la funcion obterEstado que contien lo siguiente:
+        Si nivelCarga es menor que 0 da error
+        Si nivelCarga es menor que 50 da Baixa
+        Si nivelCarga es menor que 80 da Media
+        Si nivelCarga es menor que 100 da Alta
+
+*/
     public String obterEstado() {
 
         if (nivelCarga < 0) {
